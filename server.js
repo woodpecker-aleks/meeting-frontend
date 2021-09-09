5f96d85ccb553bcd9ac980f035257833b768adc8
Aleksander Dyatlov <woodpecker.aleks@gmail.com>
2021-09-09 20:17:48 +0300

    Added route for heroku server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const serveStatic = require('serve-static')
 const path = require('path')
 const port = process.env.PORT || 3000
 
 app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
 
 app.listen(port)
